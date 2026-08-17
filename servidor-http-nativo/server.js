aca6e12b706143204097173cc720e736890ca0b9
Paulozero99 <opaulohenrique363@gmail.com>
2026-08-17 16:53:04 -0400

    atual

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,14 +1,34 @@
-const http = require("node:http")
+import http from 'node:http'
+import { URL } from 'node:url'
+
 const PORTA = 3000
+const produtos = [
+    {id: 1, nome: "Sabonete"},
+    {id: 2, nome: "Volante Logitech"},
+    {id: 3, nome: "Sabão em Pó"},
+    {id: 4, nome: "Pelucia do Sonic"}
+]
 
 //res de response e req de request
 const server = http.createServer((req, res) => {
-    console.log(`Requisão recebida! ${new Date().toISOString()} ${req.method} ${req.url}`)
-    
-    res.statusCode = 201
+    res.statusCode = 200
     res.setHeader('Content-Type', 'application/json; charset=utf-8')
 
-    res.end(JSON.stringify({ status: "ok" }))
+    if(req.method == "GET" && req.url == "/contato"){
+        return res.end(
+            JSON.stringify([
+                {data: {"número_telefone": "67-99999-9999", endereco: "Rua Alegria, 99, Centro"}}
+            ])
+        )
+    }
+
+    if(req.method == "GET" && req.url == "/produtos"){
+        return res.end(
+            JSON.stringify(produtos)
+        )
+    }
+
+    res.end(JSON.stringify({data: "Página inicial"}))
 })
 
 server.listen(PORTA, () => {
